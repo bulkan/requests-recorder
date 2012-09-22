d20d6c315067c80e08271fb6603913424d6f3a23
Bulkan Evcimen <bulkan@gmail.com>
2012-09-23 01:06:44 +1000

    started working on do_POST
    * at the moment it doesn't really work with Django auth & CSRF

--- httpproxy.py
+++ httpproxy.py
@@ -1,24 +1,70 @@
 import SocketServer
-
+import json
+import cgi
 import SimpleHTTPServer
 from urlparse import urlparse
 import signal
-import urllib
+from urllib import urlencode
+
 import requests
 
 
 PORT = 8000
+
+log = open("log.out", "w")
+
+
+def write_request(method, headers, path, data=None):
+    """ write the request to the file """
+    request = {"method": method,
+            "headers": headers,
+            "path": path,
+            "data": data
+            }
+
+    log.write(json.dumps(request))
+    log.write("\n")
 
 
 class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         (scm, netloc, path, params, query, fragment) = urlparse(
             self.path, 'http')
-        print self.path
-        resp = requests.get(self.path)
-        #print type(resp.raw)
-        #print dir(resp.raw):
-        #self.copyfile(resp.raw, self.wfile)
+        self.log_request()
+        write_request("GET", self.headers.items(), self.path)
+        resp = requests.get(self.path, headers=self.headers)
+        self.wfile.write(resp.content)
+
+    def do_POST(self):
+        self.log_request()
+        #from StringIO import StringIO
+        #d = StringIO(self.rfile.read())
+        headers = dict(self.headers.items())
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': headers['content-type'],
+             })
+
+        data = dict([(i.name, i.value) for i in form.list])
+        #write_request("POST", self.headers.items(), self.path, data=data)
+
+
+        if 'proxy-connection' in headers:
+            del headers['proxy-connection']
+
+        if 'content-length' in headers:
+            del headers['content-length']
+
+
+
+        print headers
+
+        #headers = {'cookie': headers.get("cookie"),
+                   #'content-type': headers.get('content-type')}
+
+        resp=  requests.post(self.path, headers=headers, data=data)
         self.wfile.write(resp.content)
 
 
